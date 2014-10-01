9827644e9ac3a86e921b5e025eeb72038c01a769
Huy Phan <dachuy@gmail.com>
2014-10-01 10:30:45 +0800

    Added whois.aeda.net support

--- yawhois/parser/whois_aeda_net_ae.py
+++ yawhois/parser/whois_aeda_net_ae.py
@@ -1,4 +1,31 @@
+import re
 from .base import ParserBase
+from ..exceptions import ParserError
+from ..record import Nameserver
 
 class WhoisAedaNetAeParser(ParserBase):
-    pass
+
+    @property
+    def status(self):
+        pattern = re.compile("Status:\s+(.+?)\n")
+        match   = pattern.search(self.content_for_scanner)
+        if match:
+            status = match.group(1)
+            if status == "ok":
+                return "registered"
+            raise ParserError("Unknown status '%s'." % status)
+
+        return "available"
+
+    @property
+    def available(self):
+        return self.content_for_scanner.strip() == "No Data Found"
+
+    @property
+    def registered(self):
+        return not self.available
+
+    @property
+    def nameservers(self):
+        pattern = re.compile("Name Server:\s+(.+)\n")
+        return [Nameserver(name = name) for name in  pattern.findall(self.content_for_scanner)]
