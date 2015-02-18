099b0a34af955fcb4f7b28f2280555ec811fb1e5
Huy Phan <dachuy@gmail.com>
2015-02-19 05:04:08 +0800

    Added a new bunch of scanners and parsers

--- yawhois/parser/whois_1und1_info.py
+++ yawhois/parser/whois_1und1_info.py
@@ -2,5 +2,4 @@ from ..scanner.base_icann_compliant import BaseIcannCompliantScanner
 from .base_icann_compliant import BaseIcannCompliantParser
 
 class Whois1und1InfoParser(BaseIcannCompliantParser):
-
     _scanner = BaseIcannCompliantScanner, { "pattern_available": "Domain (.+) is not registered here."}
